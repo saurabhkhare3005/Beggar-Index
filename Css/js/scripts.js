468952987182ecaa8d79965afeec65cf8943a906
saurabhkhare3005 <saurabhkhare.3005@gmail.com>
2025-03-05 00:12:40 +0530

    Delete Css/js directory

--- Css/js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Simulate live token price and market cap update
-    setInterval(function() {
-        document.getElementById('token-price').innerText = "Token Price: $0.01";
-        document.getElementById('market-cap').innerText = "Market Cap: $10,000";
-    }, 1000);
-});
